8e32079e14208791355eac9389f4810ce935d6c0
saiful-4321 <bd.saif321@gmail.com>
2022-12-17 00:18:43 +0600

    learned memory management

--- 05-time/main.go
+++ 05-time/main.go
@@ -21,5 +21,4 @@ func main() {
 	// month, day, hour, munites, secound, nano sec, time zone
 	createdDate := time.Date(2020, time.January, 11, 23, 23, 0, 0, time.UTC)
 	fmt.Println(createdDate.Format("01-02-2006 15-04-05 Monday"))
-
 }
