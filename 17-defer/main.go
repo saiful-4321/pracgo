1270a80452dc67add5e814063df6bd1317bab999
saiful-4321 <bd.saif321@gmail.com>
2022-12-20 13:21:02 +0600

    larned basic defer

---
+++ 17-defer/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+	defer fmt.Println("defer exaples1")
+	defer fmt.Println("defer exaples2")
+	fmt.Println("Welcome to defer section")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("Number is: %v\n",i)
+	}
+}
