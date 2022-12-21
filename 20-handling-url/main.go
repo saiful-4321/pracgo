eb6656c5ed7114170237c2fc31b8402231791894
saif-4321 <bd.saif321@gmail.com>
2022-12-21 22:42:56 +0600

    learned url parsing in golang

---
+++ 20-handling-url/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=react&id=10"
+
+func main() {
+	fmt.Println("Welcome to URL section")
+
+	fmt.Println(myurl)
+
+	// parsing url
+	result, _ := url.Parse(myurl)
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	// depth in query param
+	qparams := result.Query()
+	fmt.Printf("The type of the query params is : %T \n", qparams)
+
+	for _, val := range qparams {
+		fmt.Println("Param is: ", val)
+	}
+}
