42d68978afc9291d955a325f8bf3555aea08cbf1
saif-4321 <bd.saif321@gmail.com>
2022-12-22 16:16:13 +0600

    learned basic post request with form data

--- 22-post-request/main.go
+++ 22-post-request/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("welcome to post request section")
 
-	PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformPostJsonRequest() {
@@ -18,9 +19,11 @@ func PerformPostJsonRequest() {
 
 	// fake json data
 	requestBody := strings.NewReader(`
-		"coursename" : "javascript",
-		"courseid" : 10,
-		"price" : 100
+		{
+			"coursename" : "javascript",
+			"courseid" : 10,
+			"price" : 100
+		}
 	`)
 
 	response, err := http.Post(myurl, "application/json", requestBody)
@@ -31,6 +34,28 @@ func PerformPostJsonRequest() {
 
 	defer response.Body.Close()
 
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	// fake json generate for request data
+
+	data := url.Values{}
+
+	data.Add("name", "Saiful islam")
+	data.Add("email", "bd.saif321@gmail.com")
+	data.Add("mobile", "01644331161")
+
+	response, err := http.PostForm(myurl, data)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
 	content, _ := ioutil.ReadAll(response.Body)
 
 	fmt.Println(string(content))
