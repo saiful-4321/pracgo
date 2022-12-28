8cd79daf993de440ddc1814fd21856b640cac41f
Md. Saiful Islam <bd.saif321@gmail.com>
2022-12-28 22:35:01 +0600

    mongo initial setup done

---
+++ 26-mongoapi/controllers/primaryController.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const connectionString = "mongodb+srv://root:bd.saif@4321#@cluster0.wiunfmj.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// Most Important
+var collection *mongo.Collection
+
+// Connect with mongo db
+func init() {
+	//  client option
+
+}
